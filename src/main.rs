ee693dd476fd78132fdc369747077d2b0c2d4831
morkvinaValeria <lerochka877@gmail.com>
2023-02-19 15:09:44 +0200

    feat(lab1): concate network data with port

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use getopt::Opt;
 
 fn main() {
-    // args = ["network_address", "network_name", "port"];
     let mut args: Vec<String> = std::env::args().collect();
     let mut opts = getopt::Parser::new(&args, "a:b:c:");
 
-    let mut a_flag = String::new();
-    let mut b_flag = String::new();
-    let mut c_flag = String::new();
+    let mut network_address = String::new();
+    let mut network_name = String::new();
+    let mut port = String::new();
 
     loop {
         match opts.next().transpose()? {
             None => break,
             Some(opt) => match opt {
-                Opt('a', Some(arg)) => a_flag = arg.clone(),
-                Opt('b', Some(arg)) => b_flag = arg.clone(),
-                Opt('c', Some(arg)) => c_flag = arg.clone(),
+                Opt('a', Some(arg)) => network_address = arg.clone(),
+                Opt('b', Some(arg)) => network_name = arg.clone(),
+                Opt('c', Some(arg)) => port = arg.clone(),
                 _ => unreachable!(),
             },
         }
     }
-
-    let new_args = args.split_off(opts.index());
-
-
-    println!("{new_args}");
+    let network_address_plus_port = [network_address, port].join(":");
+    let network_name_plus_port = [network_name, port].join(":");
+    println!("{}", network_address_plus_port);
+    println!("{}", network_name_plus_port);
 }
