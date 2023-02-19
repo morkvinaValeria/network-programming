d2dbc0d558c4f66c6919ca56c9e76cd915b33b40
morkvinaValeria <lerochka877@gmail.com>
2023-02-19 16:13:03 +0200

    chore(lab1): add logic with colon sign

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,9 @@ fn main() {
             Some(sock_addr) => {
                 let s_ip = sock_addr.ip().to_string();
                 let s_port = sock_addr.port().to_string();
-                println!("Socket address:\n address: {s_ip}, port: {s_port}")
+                if(s_ip.contains(':')){
+                    println!("\nSocket address:\n address: {s_ip}, port: {s_port}")
+                }
             }
         }
     }
