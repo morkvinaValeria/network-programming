8af5ab0583e9d340cf2a7f0d86f6b0b52f0f0f1b
morkvinaValeria <lerochka877@gmail.com>
2023-02-19 15:46:01 +0200

    chore(lab1): socketAddress logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use getopt::Opt;
+use std::net::{SocketAddr, ToSocketAddrs};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -21,4 +22,17 @@ fn main() {
 
     let node_plus_port = [node, port].join(":");
     println!("{}", node_plus_port);
+
+    let mut addrs_iter = node_plus_port.to_socket_addrs().unwrap();
+    loop{
+        match addrs_iter.next().transpose() {
+            Ok(None) => break,
+            Ok(Some(sock_addr)) => {
+                let  = sock_addr.ip().to_string();
+                let s_port = sock_addr.port().to_string();
+                println!("Socket address:\n address: {s_ip}, port: {s_port}")
+            }
+            Err(_) => println!("Input is invalid")
+        }
+    }
 }
